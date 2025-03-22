ff964e14b8001148bf83e3c95a116c84dfab98d2
Giuseppe Turitto <giuseppe@turitto.net>
2025-03-22 09:32:23 +0100

    Finalize v1.5.2: Update compiled files and release assets

--- out/codeFlattener.js
+++ out/codeFlattener.js
@@ -15,23 +15,13 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CodeFlattener = void 0;
 const fs = __importStar(require("fs"));
@@ -138,7 +128,7 @@ Project Directory: ${workspacePath}
                         cwd: workspacePath,
                         absolute: true,
                         nodir: true,
-                        ignore: excludePatterns, // Use built-in exclude pattern support
+                        ignore: excludePatterns,
                         silent: true // Skip permission errors
                     }));
                     const matchResults = await Promise.all(matchPromises);
@@ -389,99 +379,99 @@ Project Directory: ${workspacePath}
         // List of extensions to process
         const allowedExts = [
             // Code files - Compiled Languages
-            '.c', '.h', // C
-            '.cpp', '.cxx', '.cc', '.hpp', '.hxx', // C++
-            '.cs', // C#
-            '.java', // Java
-            '.go', // Go
-            '.rs', // Rust
-            '.scala', // Scala
-            '.kt', '.kts', // Kotlin
-            '.swift', // Swift
-            '.m', '.mm', // Objective-C
-            '.dart', // Dart/Flutter
-            '.ex', '.exs', // Elixir
-            '.erl', '.hrl', // Erlang
-            '.vb', '.vbs', // Visual Basic
-            '.fs', '.fsx', '.fsi', // F#
-            '.ada', '.adb', '.ads', // Ada
-            '.d', // D
-            '.zig', // Zig
+            '.c', '.h',
+            '.cpp', '.cxx', '.cc', '.hpp', '.hxx',
+            '.cs',
+            '.java',
+            '.go',
+            '.rs',
+            '.scala',
+            '.kt', '.kts',
+            '.swift',
+            '.m', '.mm',
+            '.dart',
+            '.ex', '.exs',
+            '.erl', '.hrl',
+            '.vb', '.vbs',
+            '.fs', '.fsx', '.fsi',
+            '.ada', '.adb', '.ads',
+            '.d',
+            '.zig',
             // Code files - Scripting Languages
-            '.js', '.mjs', '.cjs', // JavaScript
-            '.ts', '.tsx', '.cts', '.mts', // TypeScript
-            '.jsx', // React JSX
-            '.py', '.pyi', '.pyw', // Python
-            '.php', '.phtml', '.php3', '.php4', // PHP
-            '.rb', '.rbw', // Ruby
-            '.pl', '.pm', // Perl
-            '.sh', '.bash', '.zsh', '.fish', // Shell scripts
-            '.ps1', '.psm1', '.psd1', // PowerShell
-            '.r', '.rmd', // R
-            '.lua', // Lua
-            '.groovy', // Groovy
-            '.tcl', // Tcl
+            '.js', '.mjs', '.cjs',
+            '.ts', '.tsx', '.cts', '.mts',
+            '.jsx',
+            '.py', '.pyi', '.pyw',
+            '.php', '.phtml', '.php3', '.php4',
+            '.rb', '.rbw',
+            '.pl', '.pm',
+            '.sh', '.bash', '.zsh', '.fish',
+            '.ps1', '.psm1', '.psd1',
+            '.r', '.rmd',
+            '.lua',
+            '.groovy',
+            '.tcl',
             // Web files
-            '.html', '.htm', '.xhtml', // HTML
-            '.css', // CSS
-            '.scss', '.sass', // SASS
-            '.less', // LESS
-            '.svg', // SVG
-            '.vue', // Vue
-            '.svelte', // Svelte
-            '.astro', // Astro
+            '.html', '.htm', '.xhtml',
+            '.css',
+            '.scss', '.sass',
+            '.less',
+            '.svg',
+            '.vue',
+            '.svelte',
+            '.astro',
             // Data and Config files
-            '.json', '.jsonc', '.json5', // JSON
-            '.xml', '.xsd', '.dtd', // XML
-            '.yaml', '.yml', // YAML
-            '.toml', // TOML
-            '.ini', // INI
-            '.conf', '.config', // Config
-            '.properties', // Properties
-            '.lock', // Lock files
-            '.sql', '.mysql', '.pgsql', '.sqlite', // SQL
-            '.graphql', '.gql', // GraphQL
-            '.csv', '.tsv', // CSV
+            '.json', '.jsonc', '.json5',
+            '.xml', '.xsd', '.dtd',
+            '.yaml', '.yml',
+            '.toml',
+            '.ini',
+            '.conf', '.config',
+            '.properties',
+            '.lock',
+            '.sql', '.mysql', '.pgsql', '.sqlite',
+            '.graphql', '.gql',
+            '.csv', '.tsv',
             // Infrastructure and DevOps
-            '.tf', '.tfvars', '.hcl', // Terraform/HCL
-            '.dockerfile', 'Dockerfile', // Dockerfile (no extension)
-            '.docker-compose.yml', 'docker-compose.yml', // Docker Compose
-            '.k8s.yaml', '.kubernetes.yaml', // Kubernetes manifests
-            '.helm', // Helm charts
-            '.nix', // Nix
-            '.bicep', // Azure Bicep
-            '.cf.json', '.cf.yaml', // CloudFormation
-            '.cdktf.json', // CDK for Terraform
+            '.tf', '.tfvars', '.hcl',
+            '.dockerfile', 'Dockerfile',
+            '.docker-compose.yml', 'docker-compose.yml',
+            '.k8s.yaml', '.kubernetes.yaml',
+            '.helm',
+            '.nix',
+            '.bicep',
+            '.cf.json', '.cf.yaml',
+            '.cdktf.json',
             // Documentation
-            '.md', '.markdown', // Markdown
-            '.txt', // Text
-            '.rst', // reStructuredText
-            '.adoc', // AsciiDoc
-            '.tex', '.latex', // LaTeX
-            '.texi', // Texinfo
+            '.md', '.markdown',
+            '.txt',
+            '.rst',
+            '.adoc',
+            '.tex', '.latex',
+            '.texi',
             // Project files
-            '.sln', // VS Solution
-            '.csproj', '.vbproj', '.vcxproj', // Visual Studio projects
-            '.fsproj', // F# project
-            '.gradle', // Gradle
-            '.pom', // Maven
-            '.cargo', // Cargo (Rust)
-            '.project', // Eclipse
-            '.pbxproj', // Xcode
-            '.bazel', '.bzl', // Bazel
-            '.cmake', // CMake
-            'Makefile', '.makefile', // Make
-            '.cabal', // Haskell Cabal
+            '.sln',
+            '.csproj', '.vbproj', '.vcxproj',
+            '.fsproj',
+            '.gradle',
+            '.pom',
+            '.cargo',
+            '.project',
+            '.pbxproj',
+            '.bazel', '.bzl',
+            '.cmake',
+            'Makefile', '.makefile',
+            '.cabal',
             // Mobile & Frontend Frameworks
-            '.xcodeproj', '.pbxproj', // iOS
-            '.storyboard', '.xib', // iOS UI
-            '.gradle', '.xml', // Android
-            '.dart', // Flutter
+            '.xcodeproj', '.pbxproj',
+            '.storyboard', '.xib',
+            '.gradle', '.xml',
+            '.dart',
             // Other important code formats
-            '.wasm', // WebAssembly
-            '.asm', '.s', // Assembly
-            '.proto', // Protocol Buffers
-            '.avsc', // Avro Schema
+            '.wasm',
+            '.asm', '.s',
+            '.proto',
+            '.avsc',
             '.thrift' // Thrift
         ];
         return allowedExts.includes(ext) || ext === '';
@@ -500,9 +490,9 @@ Project Directory: ${workspacePath}
         if (['.js', '.jsx', '.ts', '.tsx', '.mjs', '.cjs', '.mts', '.cts'].includes(ext)) {
             // JavaScript/TypeScript imports
             const importRegexes = [
-                /import\s+.*?from\s+['"]([^'"]+)['"];?/g, // ES6 imports
-                /import\s*\(['"]([^'"]+)['"]\);?/g, // Dynamic imports
-                /require\s*\(['"]([^'"]+)['"]\);?/g, // CommonJS require
+                /import\s+.*?from\s+['"]([^'"]+)['"];?/g,
+                /import\s*\(['"]([^'"]+)['"]\);?/g,
+                /require\s*\(['"]([^'"]+)['"]\);?/g,
                 /import\s+['"]([^'"]+)['"];?/g // Side-effect imports
             ];
             for (const regex of importRegexes) {
@@ -517,7 +507,7 @@ Project Directory: ${workspacePath}
         else if (['.py', '.pyi', '.pyw'].includes(ext)) {
             // Python imports
             const importRegexes = [
-                /import\s+([\w\d_.]+)/g, // import x
+                /import\s+([\w\d_.]+)/g,
                 /from\s+([\w\d_.]+)\s+import/g // from x import y
             ];
             for (const regex of importRegexes) {
@@ -563,7 +553,7 @@ Project Directory: ${workspacePath}
         else if (['.rs'].includes(ext)) {
             // Rust imports
             const importRegexes = [
-                /use\s+([\w\d_:]+(?:::\{[\w\d_,\s:]+\})?)(?:;|\s)/g, // use path::to::thing
+                /use\s+([\w\d_:]+(?:::\{[\w\d_,\s:]+\})?)(?:;|\s)/g,
                 /extern\s+crate\s+([\w\d_]+)(?:;|\s)/g // extern crate thing
             ];
             for (const regex of importRegexes) {
@@ -639,7 +629,7 @@ Project Directory: ${workspacePath}
         else if (['.dart'].includes(ext)) {
             // Dart imports
             const importRegexes = [
-                /import\s+['"](.+?)['"](?:\s+as\s+[\w\d_]+)?;/g, // import 'package:flutter/material.dart';
+                /import\s+['"](.+?)['"](?:\s+as\s+[\w\d_]+)?;/g,
                 /part\s+['"](.+?)['"];/g // part 'file.dart';
             ];
             for (const regex of importRegexes) {
@@ -718,8 +708,8 @@ Project Directory: ${workspacePath}
         else if (['.ex', '.exs'].includes(ext)) {
             // Elixir imports
             const importRegexes = [
-                /alias\s+([\w\d_.]+)(?:\.[\w\d_]+)?/g, // alias Module.Name
-                /import\s+([\w\d_]+)(?:\.\{[\w\d_,\s]+\})?/g, // import Module
+                /alias\s+([\w\d_.]+)(?:\.[\w\d_]+)?/g,
+                /import\s+([\w\d_]+)(?:\.\{[\w\d_,\s]+\})?/g,
                 /require\s+([\w\d_]+)/g // require Logger
             ];
             for (const regex of importRegexes) {
